86aff307a6d8efcaebc6c048d4ea9acfbef440e3
AymenGhazouani <aymeeen.gh@gmail.com>
2025-05-13 13:44:45 +0100

    fixed lucky wheel

--- src/components/wheel/WheelFortuneDialog.tsx
+++ src/components/wheel/WheelFortuneDialog.tsx
@@ -1,22 +1,23 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { WheelOfFortune } from "./WheelOfFortune"; // The component we created earlier
+import { WheelOfFortune } from "./WheelOfFortune";
+import wheelIcon from '@/assets/icons/wheel-icon.png';
 
 export function WheelFortuneDialog() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button variant="outline" size="sm" className="flex items-center gap-2">
+                <Button className="daily-spin-button flex items-center gap-2 " >
                     <img
-                        src="/wheel-icon.png" // Add your wheel icon
-                        alt="Wheel"
-                        className="w-5 h-5"
+                        src={wheelIcon}
+                        alt="Daily Spin"
+                        className="w-6 h-6 object-contain"
                     />
                     Daily Spin
                 </Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[600px]">
-                <WheelOfFortune />
+                <WheelOfFortune/>
             </DialogContent>
         </Dialog>
     );
